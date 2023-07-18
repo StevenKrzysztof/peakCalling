8c6671678695d7a9048650d283f5b1d7e8b429e8
Steven Krzysztof <48268079+StevenKrzysztof@users.noreply.github.com>
2023-07-18 15:42:35 -0700

    without any bugs

--- evaluate_models/evaluate_epic2.sh
+++ evaluate_models/evaluate_epic2.sh
@@ -22,8 +22,8 @@ calculate_f1_score() {
 
 threshold() {
     counts=$1
-    predicted_truth=$(cat $sample_file_handle | awk -v s=$1 '$4 <= s')
-    predicted_false=$(cat $sample_file_handle | awk -v s=$1 '$4 > s')
+    predicted_truth=$(cat $sample_file_handle | awk -v s=$1 '$11 <= s')
+    predicted_false=$(cat $sample_file_handle | awk -v s=$1 '$11 > s')
     TP=$(echo "$predicted_truth" | cut -f1-3 | bedtools intersect -u -a - -b $standard | wc -l)
     FP=$(echo "$predicted_truth" | cut -f1-3 | bedtools intersect -v -a - -b $standard | wc -l)
     FN=$(echo "$predicted_false" | cut -f1-3 | bedtools intersect -u -a - -b $standard | wc -l)
@@ -38,13 +38,11 @@ threshold() {
     echo -e "$method\t$condition\t$replicate\t$mark\t$counts\t$TP\t$FP\t$FN\t$TN\t$precision\t$recall\t$fpr\t$f1"
 }
 
-
-
 # file I/O ------------------------------------------------------------------------------
 
 file=$(find data/epic2 -name "${sample}*Peak") # this workaround accommodates for narrowPeak and broadPeak inputs
 
-# identify method, condition, replicate, mark
+# identify method,condition,replicate,mark
 method="epic2"
 condition=$(basename $file | cut -d_ -f1)
 replicate=$(basename $file | cut -d_ -f2)
@@ -56,8 +54,8 @@ echo -e "method\tcondition\treplicate\tmark\tsignal\tTP\tFP\tFN\tTN\tprecision\t
 
 # create a new column with pval in decimal form. Sort by that new column.
 # identify all pvals within a sample.
-sample_file=$(awk -v OFS='\t' '{print $0,10**-$4}' $file | grep -vi "inf" | sort -rg -k4)
-sample_pvals=$(echo "$sample_file" | cut -f4 | uniq)
+sample_file=$(awk -v OFS='\t' '{print $0,10**-$4}' $file | grep -vi "inf" | sort -rg -k11)
+sample_pvals=$(echo "$sample_file" | cut -f11 | uniq)
 sample_file_handle="data/evaluate_models/${method}_${condition}_${replicate}_${mark}.tmp.txt" # create tmp file to threshold from
 echo "$sample_file" > $sample_file_handle
 
